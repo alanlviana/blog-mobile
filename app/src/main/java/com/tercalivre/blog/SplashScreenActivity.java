b7c0f3675646ba71bdae1c4d1561734f72594ef6
alanlviana <alanlviana@hotmail.com>
2017-04-26 23:15:12 -0300

    Tela de Leitura trabalha com Fragments
    Removidos imports e variáveis não utilizadas.

--- app/src/main/java/com/tercalivre/blog/SplashScreenActivity.java
+++ app/src/main/java/com/tercalivre/blog/SplashScreenActivity.java
@@ -3,24 +3,14 @@ package com.tercalivre.blog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 
 public class SplashScreenActivity extends AppCompatActivity {
-
-    private View main;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_splash_screen);
-
-        main = findViewById(R.id.splash_main);
 
         Handler handle = new Handler();
         handle.postDelayed(new Runnable() {
